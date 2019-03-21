5d087f49cb3d1d7a821bd8f13f1d4fca7708e8b2
jerryzj <jerry0715no14@gmail.com>
2019-03-21 16:49:47 +0800

    add type checking

--- HW1/code/filter_util.py
+++ HW1/code/filter_util.py
@@ -8,6 +8,10 @@ def gen_gaussian_kernel(kernel_len, sigma = 100):
             Return:
                 Kernel (np.ndarray):
     """
+    # Type Checking
+    assert isinstance(kernel_len, int)
+    assert isinstance(sigma, int)
+
     ax = np.arange(-kernel_len // 2 + 1., kernel_len // 2 + 1.)
     xx, yy = np.meshgrid(ax, ax)
     kernel = np.exp(-0.5 * (np.square(xx) + np.square(yy)) / np.square(sigma))
